07174c64e30b7490cf386f7b8e9c77e7174fae71
Donny Pratama <donnypratama1024@gmail.com>
2020-08-16 14:59:59 +0700

    add new optional properties

--- lib/app/components/badge.dart
+++ lib/app/components/badge.dart
@@ -2,21 +2,25 @@ import 'package:flutter/material.dart';
 
 class Badge extends StatelessWidget {
   final Color color;
+  final EdgeInsetsGeometry padding;
+  final BorderRadiusGeometry borderRadius;
   final Widget child;
 
   const Badge({
     Key key,
     this.color,
+    this.padding = const EdgeInsets.symmetric(horizontal: 8, vertical: 4),
+    this.borderRadius,
     @required this.child,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 4),
+      padding: this.padding,
       decoration: BoxDecoration(
         color: this.color ?? Theme.of(context).primaryColor,
-        borderRadius: BorderRadius.circular(8.0),
+        borderRadius: this.borderRadius ?? BorderRadius.circular(8.0),
       ),
       child: this.child,
     );
